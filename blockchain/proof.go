cb21e05138bbe4366bee40177feef8919f6e53ff
brizenchi <kuniseichi@gmail.com>
2023-05-22 08:44:39 +0800

    transaction

--- blockchain/proof.go
+++ blockchain/proof.go
@@ -10,6 +10,17 @@ import (
 	"math/big"
 )
 
+// Take the data from the block
+
+// create a counter (nonce) which starts at 0
+
+// create a hash of the data plus the counter
+
+// check the hash to see if it meets a set of requirements
+
+// Requirements:
+// The First few bytes must contain 0s
+
 const Difficulty = 12
 
 type ProofOfWork struct {
@@ -20,34 +31,32 @@ type ProofOfWork struct {
 func NewProof(b *Block) *ProofOfWork {
 	target := big.NewInt(1)
 	target.Lsh(target, uint(256-Difficulty))
-	pow := &ProofOfWork{b, target}
-	return pow
-}
 
-func ToHex(num int64) []byte {
-	buff := new(bytes.Buffer)
-	err := binary.Write(buff, binary.BigEndian, num)
-	if err != nil {
-		log.Panic(err)
-	}
-	return buff.Bytes()
+	pow := &ProofOfWork{b, target}
+
+	return pow
 }
 
 func (pow *ProofOfWork) InitData(nonce int) []byte {
 	data := bytes.Join(
 		[][]byte{
 			pow.Block.PrevHash,
-			pow.Block.Data,
+			pow.Block.HashTransactions(),
 			ToHex(int64(nonce)),
-			ToHex(Difficulty),
+			ToHex(int64(Difficulty)),
 		},
-		[]byte{})
+		[]byte{},
+	)
+
 	return data
 }
+
 func (pow *ProofOfWork) Run() (int, []byte) {
 	var intHash big.Int
 	var hash [32]byte
+
 	nonce := 0
+
 	for nonce < math.MaxInt64 {
 		data := pow.InitData(nonce)
 		hash = sha256.Sum256(data)
@@ -60,13 +69,31 @@ func (pow *ProofOfWork) Run() (int, []byte) {
 		} else {
 			nonce++
 		}
+
 	}
+	fmt.Println()
+
 	return nonce, hash[:]
 }
+
 func (pow *ProofOfWork) Validate() bool {
 	var intHash big.Int
+
 	data := pow.InitData(pow.Block.Nonce)
+
 	hash := sha256.Sum256(data)
 	intHash.SetBytes(hash[:])
+
 	return intHash.Cmp(pow.Target) == -1
+}
+
+func ToHex(num int64) []byte {
+	buff := new(bytes.Buffer)
+	err := binary.Write(buff, binary.BigEndian, num)
+	if err != nil {
+		log.Panic(err)
+
+	}
+
+	return buff.Bytes()
 }
